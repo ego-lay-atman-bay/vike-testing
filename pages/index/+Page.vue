3bc6780798492a38f59a1a1aa5983df195c8bcf7
ego-lay_atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2026-03-23 14:41:27 -0600

    Create save as demo

---
+++ pages/index/+Page.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { useEventListener } from '@vueuse/core';
+import { ref } from 'vue';
+
+useEventListener('beforeunload', (e) => {
+    if (isDirty.value) {
+        e.preventDefault()
+    }
+})
+
+const isDirty = ref<boolean>(false)
+
+</script>
+
+<template>
+  <h1>My Vike app</h1>
+  <p>
+    <label>
+      <input v-model="isDirty" type="checkbox" name="toggle-dirty">
+      Toggle dirty
+    </label>
+  </p>
+</template>
