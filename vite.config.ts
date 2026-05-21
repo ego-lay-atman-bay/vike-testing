4914459cb486d116eb508f5ad0900eff51f25825
ego-lay_atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2026-05-21 12:54:18 -0600

    Scaffold new app

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { cloudflare } from "@cloudflare/vite-plugin";
+import vue from "@vitejs/plugin-vue";
+/// <reference types="@batijs/core/types" />
+
+import vike from "vike/plugin";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [
+    vike(),
+    vue(),
+    cloudflare({
+      viteEnvironment: {
+        name: "ssr",
+      },
+    }),
+  ],
+});
